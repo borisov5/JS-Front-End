4a2082697fa55079821ab8bbdbc351d7a4123552
Ivelin Borisov <borisov1990@gmail.com>
2023-03-08 21:45:53 +0200

    added 8th task

---
+++ 02. Functions and Statements/Exercise/08. Perfect Number.js
@@ -0,0 +1,20 @@
+function isPerfect(number) {
+    let divisors = [];
+
+    for (let currentNum = 1; currentNum < number; currentNum++) {
+        if (number % currentNum === 0) {
+            divisors.push(currentNum);
+        }
+    }
+
+    let divisorsSum = divisors.reduce((previousVal, currentValue) => previousVal + currentValue, 0);
+
+    if (number === divisorsSum) {
+        console.log('We have a perfect number!');
+    }   else {
+        console.log('It\'s not so perfect.');
+    }
+}
+
+isPerfect(6);
+isPerfect(28);
