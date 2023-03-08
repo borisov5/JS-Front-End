eb8a6553c296dcd5b5f98becd23e886e9633a187
Ivelin Borisov <borisov1990@gmail.com>
2023-03-08 21:44:55 +0200

    added 3rd task

---
+++ 02. Functions and Statements/Exercise/03. Characters in Range.js
@@ -0,0 +1,20 @@
+function charactersInRange(firstChar, secondChar) {
+    const getFirstCharAsciiCode = (char) => char.charCodeAt(0);
+    let firstCharAscii = getFirstCharAsciiCode(firstChar);
+    let secondCharAscii = getFirstCharAsciiCode(secondChar);
+
+    let minAscii = Math.min(firstCharAscii, secondCharAscii);
+    let maxAscii = Math.max(firstCharAscii, secondCharAscii);
+    let chars = [];
+
+    for (let asciiCode = minAscii + 1; asciiCode < maxAscii; asciiCode++) {
+        chars.push(String.fromCharCode(asciiCode));
+    }
+
+    return chars.join(' ');
+}
+
+console.log(charactersInRange(
+    'a',
+    'd'
+))
