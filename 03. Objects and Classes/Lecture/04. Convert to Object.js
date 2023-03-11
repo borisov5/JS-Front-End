bfb3725fc71c1b2718d3e6617c4021666f1e77aa
Ivelin Borisov <borisov1990@gmail.com>
2023-03-11 20:57:16 +0200

    added 4th task

---
+++ 03. Objects and Classes/Lecture/04. Convert to Object.js
@@ -0,0 +1,8 @@
+function convertToObj(jsonString) {
+    let data = JSON.parse(jsonString);
+    for (const key in data) {
+        console.log(`${key}: ${data[key]}`);
+    }
+}
+
+convertToObj('{"name": "George", "age": 40, "town": "Sofia"}')
