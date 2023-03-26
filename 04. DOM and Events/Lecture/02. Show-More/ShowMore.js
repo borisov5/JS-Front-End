0db59fe0845b8023d5d0762b0cf6224d222659ba
Ivelin Borisov <borisov1990@gmail.com>
2023-03-26 20:44:13 +0300

    added second DOM task

---
+++ 04. DOM and Events/Lecture/02. Show-More/ShowMore.js
@@ -0,0 +1,7 @@
+function showText() {
+    const anchor = document.getElementById('more');
+    const span = document.getElementById('text');
+
+    span.style.display = 'block';
+    anchor.style.display = 'none';
+}
